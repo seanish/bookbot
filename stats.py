065c7efd491815e08a441c1cc7732f01d3f86f8d
seanish <sean.mallia@icloud.com>
2025-08-10 12:28:52 -0400

    add count_characters

--- stats.py
+++ stats.py
@@ -8,3 +8,14 @@ def count_words(book):
     text = get_book_text(book)
     words = text.split()
     return len(words)
+
+def count_characters(book):
+    text = get_book_text(book)
+    characters = {}
+    for char in text:
+        char.lower()
+        if char in characters:
+            characters[char] += 1
+        else:
+            characters.update({char: 1})
+    return characters
