63e4a2baac72267300d90bc47dcdbb3330105e7a
seanish <sean.mallia@icloud.com>
2025-08-10 13:35:35 -0400

    adding variables usage

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from stats import count_words, create_final_count
+import sys
 
 def main():
-    word_count = count_words("books/frankenstein.txt")
-    character_count = create_final_count("books/frankenstein.txt")
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt")
-    print("----------- Word Count ----------")
-    print(f"Found {word_count} total words")
-    print("--------- Character Count -------")
-    i = 0
-    while i < len(character_count) - 1:
-        i += 1
-        if character_count[i]["char"].isalpha() != True:
-            continue
-        else:
-            print(f"{character_count[i]['char']}" ": " f"{character_count[i]['num']}")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        word_count = count_words(sys.argv[1])
+        character_count = create_final_count(sys.argv[1])
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at books {sys.argv[1]}")
+        print("----------- Word Count ----------")
+        print(f"Found {word_count} total words")
+        print("--------- Character Count -------")
+        i = 0
+        while i < len(character_count) - 1:
+            i += 1
+            if character_count[i]["char"].isalpha() != True:
+                continue
+            else:
+                print(f"{character_count[i]['char']}" ": " f"{character_count[i]['num']}")
 
-    print("============= END ===============")
+        print("============= END ===============")
 
 main()
